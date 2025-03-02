3b0b03998a3361b77f03334d528939d93a86ab58
Colin Eckert <colineckert10@gmail.com>
2025-03-01 21:14:48 -0800

    print sorted list of char counts

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-from stats import get_num_words, get_char_count
+from stats import get_num_words, get_char_count, get_sorted_char_counts
 
 def get_book_text(file_path: str) -> str:
     with open(file_path) as file:
         return file.read()
 
 def main():
+    print("============ BOOKBOT ============")
+
     file_path = "books/frankenstein.txt"
+    print(f"Analyzing book found at {file_path}...")
+
     text = get_book_text(file_path)
     num_words = get_num_words(text)
+
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
     num_chars = get_char_count(text)
-    print(f"{num_words} words found in the document")
-    print(num_chars)
+    sorted_chars_list = get_sorted_char_counts(num_chars)
+    for item in sorted_chars_list:
+        char = list(item.keys())[0]
+        count = item[char]
+        print(f"{char}: {count}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -4,11 +4,20 @@ def get_num_words(text: str) -> int:
 def get_char_count(text: str) -> "dict[str, int]":
     char_dict = {}
     for char in text:
-        if char == ' ':
-            continue
-        lower_char = char.lower()
-        if lower_char in char_dict:
-            char_dict[lower_char] += 1
-        else:
-            char_dict[lower_char] = 1
+        if char.isalpha():
+            lower_char = char.lower()
+            if lower_char in char_dict:
+                char_dict[lower_char] += 1
+            else:
+                char_dict[lower_char] = 1
     return char_dict
+
+def sort_on_value(dict):
+    return list(dict.values())[0]
+
+def get_sorted_char_counts(char_dict: "dict[str, int]"):
+    char_count_list = []
+    for char, count in char_dict.items():
+        char_count_list.append({ char: count })
+    char_count_list.sort(key=sort_on_value, reverse=True)
+    return char_count_list
